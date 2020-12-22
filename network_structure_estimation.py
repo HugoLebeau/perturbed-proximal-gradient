3e7533428a2770fc788c446b9e425c8a24081b8a
Hugo Lebeau <hug97@hotmail.fr>
2020-12-22 21:15:08 +0100

    Introduce numba + compute grad_f

--- network_structure_estimation.py
+++ network_structure_estimation.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numba import njit
+from tqdm import tqdm
 from scipy.special import logsumexp
 
 from utils import vec2mat
@@ -8,21 +10,28 @@ np.random.seed(1)
 
 M = 20 # number of possible states
 p = 50 # dimension
+N = 250 # sample size
 
 # Generation of the true theta
-theta = np.zeros((p, p))
+theta_true = np.zeros((p, p))
 nb_nonzero = np.int(np.round(np.random.randn()*(p/4.)+p)) # number of non-zero elements below the diagonal
 lines = np.random.randint(1, p, nb_nonzero)
 for i in lines:
     j = np.random.randint(0, i)
     u = np.random.rand()
     sgn = np.random.randint(0, 2)*2-1
-    theta[i, j] = sgn*(3.*u+1.) # uniform on [-4, -1] U [1, 4]
-    theta[j, i] = theta[i, j]
+    theta_true[i, j] = sgn*(3.*u+1.) # uniform on [-4, -1] U [1, 4]
+    theta_true[j, i] = theta_true[i, j]
 
-B0 = lambda x: x
-B = lambda x, y: float(x == y) if (np.isscalar(x) and np.isscalar(y)) else (x == y).astype(float)
+@njit
+def B0(x):
+    return x
 
+@njit
+def B(x, y):
+    return np.where(x == y, 1, 0)
+
+@njit
 def barB(x):
     ''' Matrix of B(x_i, x_j) (B0(x_i) on the diagonal) '''
     mat = np.diag(B0(x))
@@ -32,6 +41,7 @@ def barB(x):
         mat[k+1:, k] = elems
     return mat
 
+@njit
 def logf(x, theta):
     ''' log f(x) + log Z '''
     mat = np.diag(B0(x))
@@ -39,32 +49,46 @@ def logf(x, theta):
         mat[k+1:, k] = B(x[k], x[k+1:])
     return np.sum(theta*mat)
 
-def condf(x, i, theta):
+def logcondf(x, i, theta):
     ''' Compute p(x_i|x_-i) for all x_i '''
     all_x = np.tile(x, (M, 1))
-    all_x[:, i] = np.arange(M)
+    all_x[:, i] = np.arange(M, dtype=int)
     all_logfx = np.array([logf(x, theta) for x in all_x])
     return all_logfx-logsumexp(all_logfx)
 
-def Gibbsf(theta, niter=100):
+@njit
+def sampling(proba, u=np.random.rand()):
+    ''' Sample from {0, ..., M-1} with the given distribution '''
+    s, k = proba[0], 0
+    while s < u and k < M:
+        k += 1
+        s += proba[k]
+    return k
+
+def Gibbsf(theta, niter=100, x0=np.zeros(p, dtype=int)):
     ''' Gibbs sampler to sample from f '''
-    sample_space = np.arange(M)
-    x = np.zeros(p)
-    for it in range(niter):
+    x = x0.copy()
+    u = np.random.rand(niter*p)
+    chain = np.zeros((niter, p), dtype=int)
+    for it in tqdm(range(niter)):
         for i in range(p):
-            x[i] = np.random.choice(sample_space, p=np.exp(condf(x, i, theta)))
-    return x
+            proba = np.exp(logcondf(x, i, theta))
+            x[i] = sampling(proba, u[it*p+i])
+        chain[it] = x
+    return chain
 
-def grad_f(theta, m=500, cov_prop=np.ones(p), size=100):
-    return 0
+# Generation of observations
+obs = Gibbsf(theta_true, niter=10+N)[10:]
 
-grad_f ? # implement a Gibbs sampler
+def grad_f(theta, obs, m=500, burn_in=10):
+    z = Gibbsf(theta, niter=burn_in+m)[burn_in:]
+    return np.mean([barB(xi) for xi in obs], axis=0)-np.mean([barB(zi) for zi in z], axis=0)
 
-g = lambda theta_vec: np.sum(np.abs(theta_vec))
-theta0 = np.zeros(p*(p+1)//2) # vector representation
-niter = 5*p
-gamma = 25.*/(p*np.arange(1, niter+1)**0.7)
-lambda_reg = 2.5*np.sqrt(np.log(p)/np.arange(1, niter+1))
+# g = lambda theta_vec: np.sum(np.abs(theta_vec))
+# theta0 = np.zeros(p*(p+1)//2) # vector representation
+# niter = 5*p
+# gamma = 25./(p*np.arange(1, niter+1)**0.7)
+# lambda_reg = 2.5*np.sqrt(np.log(p)/np.arange(1, niter+1))
 
-output = proximal_gradient(grad_f, g, theta0, gamma=1., lambda_=lambda_reg, niter=niter, prox_g=prox_L1)
-theta_est = vec2mat(output)
+# output = proximal_gradient(grad_f, g, theta0, gamma=1., lambda_=lambda_reg, niter=niter, prox_g=prox_L1)
+# theta_est = vec2mat(output)
